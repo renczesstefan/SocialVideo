c33982c25b2e09476d9c472b736fe20e9cc3115a
Martin Miklovič <miklovic@netgrif.com>
2020-11-09 14:34:08 +0100

    - added navigation to login and registration click

--- app/src/main/java/com/social/socialvideo/fragments/LoginFragment.kt
+++ app/src/main/java/com/social/socialvideo/fragments/LoginFragment.kt
@@ -4,12 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.social.socialvideo.R
+import com.social.socialvideo.databinding.LoginBinding
 
 class LoginFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.login, container, false)
+
+    private lateinit var binding: LoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        // Slúži na optimálnejšie nájdenie data fieldov pre fragment netreba používať R, findId...
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.login, container, false)
+        binding.loginModel = this
+
+        return binding.root
     }
+
+    fun onRegistrationClick() {
+        this.findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+    }
+
+    fun onLoginClick() {
+        this.findNavController().navigate(R.id.action_loginFragment_to_profileFragment)
+    }
+
 }
